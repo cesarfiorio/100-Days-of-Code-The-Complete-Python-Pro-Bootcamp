948bfb65b46dad2d1fcc0e8b08bf3ef44c22c546
Cesar Fiorio <110138343+cesarfiorio@users.noreply.github.com>
2024-07-08 23:48:54 +0100

    learning

--- hangman.py
+++ hangman.py
@@ -4,20 +4,33 @@ word_list = ["arvore", "bombom","chocolate"]
 
 chosen_word = random.choice(word_list)
 
+print(chosen_word)
+display = []
+word_length = len(chosen_word)
+ 
+for _ in range(word_length):
+    # or for n in chosen_word:
+    display.append('"_"')
+    # or display +="_"
+
+print(display)
+
 guess = input("Guess a letter: ").lower()
-
-#When we send the "chosen_word" inside the "for", it will check letter by letter, even if i change the name of the varible.
-#chosen_word her eis a word - str
-for letter in chosen_word:
+# range(len(chosen_word)) = will ran for many times we have letters in this word.
+for position in range(word_length):
+    letter = chosen_word[position]
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
 
-#When we send the "chosen_word" inside the "for", it will check letter by letter, even if i change the name of the varible.
-#chosen_word her eis a word - str
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+print(display)
+
+
+# #When we send the "chosen_word" inside the "for", it will check letter by letter, even if i change the name of the varible.
+# #chosen_word here is a word - str
+# for letter in chosen_word:
+#     if letter == guess:
+#         print("Right")
+#     else:
+#         print("Wrong")
+
+
